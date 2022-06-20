71e5ef07586471ec16f79ee922e9d3066778845c
jpmgoncalves12 <jpmgoncalves12@gmail.com>
2022-06-20 20:26:45 -0300

    Adicionando lógica

--- forca.c
+++ forca.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 char *initializeSecretArray(int qtd) {
     static char r[32];
@@ -15,13 +16,6 @@ char *initializeSecretArray(int qtd) {
     return r;
 }
 
-void renderPerson(int numTry) {
-    printf("\n");
-    printf("o\n");
-    printf("|\n");
-    printf("A\n");
-}
-
 void renderLetters(char *secretWord) {
     printf("\n");
     for (int cont = 0; cont < strlen(secretWord); cont++) {
@@ -30,9 +24,25 @@ void renderLetters(char *secretWord) {
     printf("\n");
 }
 
-void renderScreenElements(int numTry, char *secretWord) {
-    renderPerson(numTry);
-    renderLetters(secretWord);
+int verifyLetterInWord(
+    char actualLetter,
+    char *word,
+    char *secretWord
+) {
+    int isInLetter = 0;
+
+    for (int cont = 0; cont <= strlen(word); cont++) {
+        if (actualLetter == word[cont]) {
+            secretWord[cont] = actualLetter;
+            isInLetter++;
+        }
+    }
+
+    if(isInLetter) {
+        return 1;
+    }
+
+    return 0;
 }
 
 int main() {
@@ -42,13 +52,33 @@ int main() {
     char *secretWord;
     secretWord = initializeSecretArray(strlen(word));
 
-    int maxTrying = 3;
-    int numTry = 0;
+    char actualLetter;
 
-    renderScreenElements(
-        numTry,
-        secretWord
-    );
+    int qtdTry = 0;
+    int maxTry = 3;
+
+    int acertou;
+
+    while (qtdTry <= maxTry) {
+        printf("Erros %i de %i.\n", qtdTry, maxTry);
+
+        printf(" Letra > ");
+        scanf("%c", &actualLetter);
+        printf("\n");
+
+        acertou = verifyLetterInWord(
+            actualLetter,
+            word,
+            secretWord
+        );
+        printf("Acertou: %i\n", acertou);
+
+        if (acertou == 0) {
+            qtdTry++;
+        }
+
+        printf("%s", secretWord);
+    }
 
     return 0;
 }
